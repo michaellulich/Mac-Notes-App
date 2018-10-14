ff10ce4af01e96737dec0d6fa18253db0c0613cd
Michael Lulich <michael.lulich@gmail.com>
2018-10-14 17:39:46 -0500

    updated app.js layout  and app.css styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="app">
+        <div className="toolbar">
+          <button className="toolbar-button">New</button>
+          <button className="toolbar-button">Delete</button>
+          <input
+            className="toolbar-search"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
+        <div className="note-container">
+          <div className="note-selectors">
+            <div className="note-selector active">
+              <p className="note-selector-title">First note...</p>
+              <p className="note-selector-timestamp">Timestamp here...</p>
+            </div>
+            <div className="note-selector">
+              <p className="note-selector-title">Second note...</p>
+              <p className="note-selector-timestamp">Timestamp here...</p>
+            </div>
+            <div className="note-selector">
+              <p className="note-selector-title">Third note...</p>
+              <p className="note-selector-timestamp">Timestamp here...</p>
+            </div>
+          </div>
+          <div className="note-editor">
+            <p className="note-editor-info">Timestamp here...</p>
+            <textarea className="note-editor-input" />
+          </div>
+        </div>
       </div>
     );
   }
